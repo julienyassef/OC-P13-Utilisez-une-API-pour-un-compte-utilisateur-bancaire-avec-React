3c1292de6b1fdbb8304d4d9e082c445168641531
julien yassef <juyassef@gmail.com>
2024-02-22 17:05:56 +0100

    modif handleSignin

--- src/handle/handleSignIn.jsx
+++ src/handle/handleSignIn.jsx
@@ -1,32 +1,43 @@
-
-import {setAuthToken, signinSuccess} from '../action/authActions';
-
+import { setAuthToken, signinSuccess } from '../action/authActions';
 import { generateUserId } from '../utils/authUtils';
+import { login, getUserProfile } from '../utils/apiService';
 
-import { login, getUserProfile} from '../utils/apiService';
-
-
-export const handleSignIn = async (email, password, dispatch) => {
+export const handleSignIn = async (username, password, dispatch) => {
   try {
     // Appel à la fonction de login du ApiService
-    const response = await login(email, password);
+    const loginResponse = await login(username, password);
 
-    if (response.body.token) {
-      const token = response.body.token;
-      dispatch(setAuthToken(token));
-      
-      // Récupération des informations utilisateur avec le token
-      const userData = await getUserProfile(token);
+    if (loginResponse.ok) {
+      // Vérifier si la requête est réussie
+      const jsonResponse = await loginResponse.json();
 
-      // Création d'un identifiant pour l'utilisateur 
-      const userId = generateUserId(userData.firstName, userData.lastName);
+      if (loginResponse.status === 200) {
+        // Si le statut est 200, la connexion a réussi
+        if (jsonResponse.body.token) {
+          // Si un token est présent dans la réponse
+          const token = jsonResponse.body.token;
+          dispatch(setAuthToken(token));
 
-      // Dispatch une action pour mettre à jour le state avec les informations de l'utilisateur
-      dispatch(signinSuccess({ ...userData, userId }));
+          // Récupérer les informations utilisateur avec le token
+          const userData = await getUserProfile(token);
 
-      return true; // Connexion réussie
+          // Création d'un identifiant pour l'utilisateur 
+          const userId = generateUserId(userData.firstName, userData.lastName);
+
+          // Dispatch une action pour mettre à jour le state avec les informations de l'utilisateur
+          dispatch(signinSuccess({ ...userData, userId }));
+
+          return true; // Connexion réussie
+        } else {
+          console.log('Aucun token dans la réponse');
+          return false; // Connexion échouée
+        }
+      } else {
+        console.log('Statut de réponse non-200');
+        return false; // Connexion échouée
+      }
     } else {
-      console.log('Aucun token dans la réponse');
+      console.log('Échec de la requête');
       return false; // Connexion échouée
     }
   } catch (error) {
