9d573aab802bbedaf8e51e38c93270d33c98ba78
julien yassef <juyassef@gmail.com>
2024-02-27 09:43:15 +0100

    ajout /id dans le chemin route de user dans app

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -28,7 +28,7 @@ function App() {
         <Routes>
             <Route path='/' element={<Home />} />
             <Route path='/signIn' element={<SignIn />} />
-            <Route path='/user' element={<User />} />
+            <Route path='/user/:id' element={<User />} />
             <Route path='/*' element={<Error />} />
         </Routes>
         <Footer />
